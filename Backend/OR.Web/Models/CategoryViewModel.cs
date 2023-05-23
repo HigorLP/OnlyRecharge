90d214c5cedc480b1bbac294e4d1f72125eec65f
Higor <dev.higorpereira@gmail.com>
2023-05-22 22:07:57 -0300

    Criação do MVC

---
+++ Backend/OR.Web/Models/CategoryViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace OR.Web.Models {
+    public class CategoryViewModel {
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+    }
+}
---
+++ Backend/OR.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace OR.Web.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
